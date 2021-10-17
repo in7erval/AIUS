ce381bfecd0ee4e21ae26914ab9dcd70332ac38c
Dmitry Yudakov <yudakov.d@mail.ru>
2021-10-17 14:10:47 +0300

    added continuous moving of snake

--- Snake/common/Actions.py
+++ Snake/common/Actions.py
@@ -2,10 +2,13 @@ from enum import Enum
 
 
 class Actions(Enum):
-    MOVE_DOWN = 'MOVE_DOWN'
-    MOVE_UP = 'MOVE_UP'
-    MOVE_LEFT = 'MOVE_LEFT'
-    MOVE_RIGHT = 'MOVE_RIGHT'
-    EXIT = 'EXIT'
-    RESET = 'RESET'
-    UNKNOWN_COMMAND = 'UNKNOWN_COMMAND'
+    MOVE_DOWN = 'MOVE_DOWN'  # движение вниз
+    MOVE_UP = 'MOVE_UP'  # движение вверх
+    MOVE_LEFT = 'MOVE_LEFT'  # движение влево
+    MOVE_RIGHT = 'MOVE_RIGHT'  # движение вправо
+    EXIT = 'EXIT'  # выход из игры
+    RESET = 'RESET'  # сброс игры
+    SPEED_UP = 'SPEED_UP'  # увеличение скорости змейки
+    SPEED_DOWN = 'SPEED_DOWN'  # уменьшение скорости змейки
+    SINGLE_KEY_MODE = 'SINGLE_KEY_MODE'  # включение/отключение режима по нажатию на клавишу
+    UNKNOWN_COMMAND = 'UNKNOWN_COMMAND'  # неизвестная команда
